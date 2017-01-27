87fa9dc4dac40fdcc4baece528eb27427f14e3d4
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-01-27 17:37:19 +0100

    Implement application factory pattern

--- flask_graphite/flask_graphite.py
+++ flask_graphite/flask_graphite.py
@@ -1,1 +1,9 @@
-# -*- coding: utf-8 -*-
+class FlaskGraphite(object):
+
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        pass
