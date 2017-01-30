333b377781014592f7e63afc38cb747ce6e5796b
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-01-30 17:43:23 +0100

    Create graphite client from application configuration

--- flask_graphite/flask_graphite.py
+++ flask_graphite/flask_graphite.py
@@ -1,3 +1,10 @@
+import logging
+
+from graphitesend.graphitesend import GraphiteClient
+
+logger = logging.getLogger("flask-graphite")
+
+
 class FlaskGraphite(object):
 
     def __init__(self, app=None):
@@ -6,4 +13,23 @@ class FlaskGraphite(object):
             self.init_app(app)
 
     def init_app(self, app):
-        pass
+        logger.info("configuring %s", app.name)
+        self.make_config(app)
+        self.setup_graphitesend()
+        logger.info("application %s successfully configured")
+
+    def make_config(self, app, namespace=None):
+        application_config = app.config.get_namespace("FLASK_GRAPHITE_")
+
+        self.carbon_config = {}
+        for key, value in application_config.items():
+            if key.startswith("carbon_"):
+                self.carbon_config[key[len("carbon_"):]] = value
+
+    def setup_graphitesend(self):
+        carbon_config = self.carbon_config
+        logger.debug("carbon configuration: %s", carbon_config)
+        host = carbon_config.pop("host")
+        port = carbon_config.pop("port")
+        self.client = GraphiteClient(graphite_server=host, graphite_port=port,
+                                     **carbon_config)
