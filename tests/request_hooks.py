42b83d589729ec3b8ae2677106a1adf799527328
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-02-24 11:28:09 +0100

    Add request hooks tests

---
+++ tests/request_hooks.py
@@ -0,0 +1,28 @@
+import pytest
+from flask import jsonify
+
+from flask_graphite.request_hooks import default_hooks
+
+
+@pytest.fixture(params=default_hooks, ids=[x.name for x in default_hooks])
+def hook(request):
+    yield request.param
+
+
+@pytest.fixture
+def hooked_app(graphitesend_client, app, hook):
+    @app.route("/foo/<int:id>/<field>")
+    def view(id, field):
+        return jsonify("{} => {}".format(id, field))
+    hook.bind(graphitesend_client).register_into(app)
+    return app
+
+
+@pytest.fixture
+def hooked_client(hooked_app):
+    return hooked_app.test_client()
+
+
+def test_dont_modify_response(hooked_client, graphitesend_client):
+    response = hooked_client.get("/foo/42/bar")
+    assert graphitesend_client.send.called
