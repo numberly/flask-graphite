29d43753f1e4a4c8a3d300fedaf1cceb526a54e5
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-03-08 12:15:14 +0100

    Update version references

--- flask_graphite/__init__.py
+++ flask_graphite/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Alexandre Bonnetain"""
 __email__ = 'alexandre.bonnetain@1000mercis.com'
-__version__ = '0.0.0'
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='flask_graphite',
-    version='0.0.0',
+    version='0.1.0',
     description="Flask-Graphite grant you the power to push useful metrics"
                 " without efforts",
     long_description=readme + '\n\n' + history,
