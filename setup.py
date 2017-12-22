8d2cbaee7e4aa6eb19bbf27537e41ce876e982fe
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-12-22 09:37:34 +0000

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='flask_graphite',
-    version='0.4.1',
+    version='0.4.2',
     description="Flask-Graphite grant you the power to push useful metrics"
                 " for each route without effort",
     long_description=readme + '\n\n' + history,
